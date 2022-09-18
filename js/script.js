8b99d6ddafb96f57eb473ab2d6e422eca42b8807
AndyQueiroz <andykeiiroz@gmail.com>
2022-09-18 19:44:57 -0300

    Ins darkmode, Rnmd calculo.js to script.js, Edit all files

--- js/script.js
+++ js/script.js
@@ -3,10 +3,23 @@
     Year: 2022
 */
 
+const darkmode = document.getElementById('darkmode');
+const lightmode = document.getElementById('lightmode');
 var firstInput = document.querySelector('#firstInput');
 var secondInput = document.querySelector('#secondInput');
 var resultOutput = document.querySelector('#resultOutput');
 
+darkmode.addEventListener("click", () => {      /* ATIVA O DARK-MODE */
+    document.body.classList.add('darkmode');
+});
+
+/* FUTURAMENTE PODERÁ SER INSERIDO NESTE SCRIPT O LOCAL STORAGE PARA SALVAR A PREFERÊNCIA DE TEMA DO USUÁRIO */
+
+lightmode.addEventListener("click", () => {     /* RESETA PARA O LIGHT-MODE */
+    window.location.reload();
+});
+
+/* ABAIXO ESTÃO AS FUNÇÕES (DAS OPERAÇÕES MATEMÁTICAS) REALIZADAS PELO SCRIPT */
 function somar() {
     resultOutput.innerHTML = parseInt(firstInput.value) + parseInt(secondInput.value);
 }
